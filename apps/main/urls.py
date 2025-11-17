6f7e438219d02b829adfe7edca7eefd60837cb36
Nikita <nikitadidenko222@gmail.com>
2025-11-17 21:23:04 +0100

    Main and catalog views and templates

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,6 +1,13 @@
+from xml.etree.ElementInclude import include
+
 from django.urls import path
-from .views import home
+from django.urls import path
+from apps.main import views as main_views
+from apps.catalog import views as catalog_views
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', main_views.Home.as_view(), name='home'),
+    path('catalog/', catalog_views.CategoryIndexView.as_view(), name='catalog:index'),
+    path('catalog/<slug:slug>/', catalog_views.ProductDetailView.as_view(), name='catalog:product'),
+
 ]
