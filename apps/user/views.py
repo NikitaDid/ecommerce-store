3eb7bff35b3aec93392d6dcf17ba63c6ef89bb83
Nikita <nikitadidenko222@gmail.com>
2025-11-23 21:02:54 +0100

    addedlogin page

---
+++ apps/user/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+
+from apps.user.forms import LoginForm
+
+
+def user_login(request):
+    error = None
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            user = authenticate(username=cleaned_data['username'], password=cleaned_data['password'])
+            if user:
+                login(request, user)
+                return redirect('home')
+        error = 'INVALID PASS OR LOGIN!!! DU HURENSOHN!'
+    return render(request, 'user/login.html', {'error': error})
+
