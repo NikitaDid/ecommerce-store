e83da72d6cab4fcf5cdd4fb47b006994607632cf
Nikita <nikitadidenko222@gmail.com>
2025-11-25 21:24:08 +0100

    user app

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,8 +43,11 @@ INSTALLED_APPS = [
     'apps.cart',
     'apps.catalog',
     'apps.main',
+    'apps.user',
+
     'mptt',
-    'imagekit'
+    'imagekit',
+    'phonenumber_field'
 ]
 
 MIDDLEWARE = [
@@ -134,3 +139,4 @@ MEDIA_ROOT = 'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'user.User'
