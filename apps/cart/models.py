52515acc6e9772fd30d992440981cbee1c81b6f1
Nikita <nikitadidenko222@gmail.com>
2025-12-19 19:18:47 +0100

    fixed product detail image

--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from apps.catalog.models import Product
+from apps.user.models import User
+
+
+class Cart(models.Model):
+    product = models.ForeignKey(Product, verbose_name="Product", on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(verbose_name='Quantity')
+    user = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Cart'
+        verbose_name_plural = 'Carts'
--- apps/catalog/models.py
+++ apps/catalog/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 from mptt.models import MPTTModel, TreeForeignKey
-from imagekit.models import  ProcessedImageField, ImageSpecField
+from imagekit.models import ProcessedImageField, ImageSpecField
 from pilkit.processors import ResizeToFill
+
+from core.settings import MEDIA_ROOT
+
 
 class Category(MPTTModel):
     name = models.CharField(verbose_name='Name', max_length=255)
@@ -17,14 +22,56 @@ class Category(MPTTModel):
     image = ProcessedImageField(
         verbose_name='Image',
         upload_to='catalog/categories',
-        processors=[ResizeToFill(600,400)],
+        processors=[ResizeToFill(600, 400)],
         null=True,
         blank=True
     )
 
+
 class Meta:
     verbose_name = 'Category'
     verbose_name_plural = 'Categories'
+
+
+class ProductImage(models.Model):
+    image = ProcessedImageField(
+        verbose_name='Image',
+        upload_to='catalog/product/',
+        processors=[]
+    )
+    image_thumbnail = ImageSpecField(
+        source='image',
+        processors=[ResizeToFill(600, 400)]
+    )
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='Main Image', default=False)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None, ):
+        if self.is_main:
+            ProductImage.objects.filter(product=self.product).update(is_main=False)
+        super().save(force_insert, force_update, using, update_fields)
+
+    def image_tag_thumbnail(self):
+        if not self.image_thumbnail:
+            ProductImage.objects.get(id=self.id)
+        return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image_thumbnail}' width='70'>")
+
+    image_tag_thumbnail.short_description = 'Current Image'
+
+    def image_tag(self):
+        if self.image:
+            if not self.image_thumbnail:
+                ProductImage.objects.get(id=self.id)
+            return mark_safe(f"<img src='/{MEDIA_ROOT}{self.image_thumbnail}'>")
+
+    image_tag.short_description = 'Current Image'
+
+    def __str__(self):
+        return ''
+
+    class Meta:
+        verbose_name = 'Product Image'
+        verbose_name_plural = 'Product Images'
 
 
 class Product(models.Model):
@@ -47,9 +94,32 @@ class Product(models.Model):
     )
     image_thumbnail = ImageSpecField(
         source='image',
-        processors=[ResizeToFill(600,400)]
+        processors=[ResizeToFill(600, 400)]
     )
 
+    def images(self):
+        return ProductImage.objects.filter(product=self.id)
+
+    def main_image(self):
+        image = ProductImage.objects.filter(product=self.id, is_main=True).first()
+        if not image:
+            image = self.images().first()
+        return image
+
+    def image_tag(self):
+        image = self.main_image()
+        if image:
+            return image.image_tag_thumbnail()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('product', args=[self.slug])
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 
 # Intermediate model connecting Category and Product
 class ProductCategory(models.Model):
@@ -63,6 +133,9 @@ class ProductCategory(models.Model):
         if self.is_main:
             ProductCategory.objects.filter(product=self.product).update(is_main=False)
         super().save(force_insert, force_update, using, update_fields)
+
+    def __str__(self):
+        return ''
 
     class Meta:
         verbose_name = 'Product Category'
