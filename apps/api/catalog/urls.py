5d05f9aa1a4af12e783b405aaf0d187c080189e8
Nikita <nikitadidenko222@gmail.com>
2026-01-05 10:42:15 +0100

    drf categories

--- apps/api/catalog/urls.py
+++ apps/api/catalog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from apps.api.catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+from apps.api.catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView, CategoryViewSet
+
+from rest_framework.routers import DefaultRouter
 
 app_name = 'catalog'
 
@@ -11,3 +13,7 @@ urlpatterns = [
     path('product/delete<int:pk>/', ProductDeleteView.as_view()),
 ]
 
+router = DefaultRouter()
+router.register('category', CategoryViewSet, basename='category' )
+
+urlpatterns += router.urls
